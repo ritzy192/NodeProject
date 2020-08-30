42a72f0a68da0e13d2e7efc37adb39814314a78d
Rythem Datta <ritzy192@gmail.com>
2020-08-30 16:59:39 +0530

    added chalk package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,10 +1,8 @@
-const validator = require('validator')
-
-const add  = require('./utils.js')
+const chalk = require('chalk')
 const notes = require('./notes')
 
-const sum = add(4,4)
+
 const note = notes()
 console.log(note)
-console.log(sum)
-console.log(validator.isEmail('ritzy192@gmail.com'))
+
+console.log(chalk.blue.bgRed.underline.inverse.bold('Hello world!'))
