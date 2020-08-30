b36b431ad6f96857349eb74ef8ba3e3212c341d3
Rythem Datta <ritzy192@gmail.com>
2020-08-30 17:36:08 +0530

    added nodemon package

--- notes-app/app.js
+++ notes-app/app.js
@@ -5,4 +5,4 @@ const notes = require('./notes')
 const note = notes()
 console.log(note)
 
-console.log(chalk.blue.bgRed.underline.inverse.bold('Hello world!'))
+console.log(chalk.blue.bgRed.underline.inverse.bold('Success'))
