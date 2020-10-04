7d7a53f8926268d72b08c85142a6e70d1373ce10
Rythem Datta <ritzy192@gmail.com>
2020-10-04 14:11:52 +0530

    removed getNotes

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,8 +1,6 @@
 const fs = require('fs')
 const chalk = require('chalk')
 
-const getNotes = () => 'Your Notes...'
-
 const addNote = (title, body) => {
     const notes = loadNotes()
     //Filter will not stop at first match and will search through complete array
@@ -71,7 +69,6 @@ const loadNotes = () => {
 }
 
 module.exports = {
-    getNotes: getNotes,
     addNote: addNote,
     removeNote: removeNote,
     listNotes: listNotes,
