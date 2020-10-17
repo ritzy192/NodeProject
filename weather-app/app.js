74fb341b5c739168f57ba10262c7578dae0c564a
Rythem Datta <ritzy192@gmail.com>
2020-10-17 16:11:15 +0530

    customising weather data result

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 const url = 'http://api.weatherstack.com/current?access_key=3b01490a702d8d41d46cba938459868c&query=Dehradun'
 
-request( {url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request( {url: url, json: true }, (error, response) => {
+    const data = response.body.current
+    console.log(data.weather_descriptions[0] + '. It is currently ' + data.temperature +' degrees out. It feels like ' + data.feelslike + ' degrees out.')
 })
