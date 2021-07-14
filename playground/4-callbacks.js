a718163ca736333033e5ccb58df80c7403235795
Rythem Datta <ritzy192@gmail.com>
2021-07-14 18:59:32 +0530

    taking address from command line

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -25,3 +25,60 @@ const add = (a,b,callback) => {
 add(1, 4, (sum) => {
     console.log(sum) // Should print: 5
 })
+
+// const request = require('request')
+// const request = require('request')
+// const geoCode = (location, callback) => {
+//     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(location) + '.json?access_token=pk.eyJ1Ijoicml0enkxOTIiLCJhIjoiY2tnZGs1YWlmMDBseTJwcGZyYXE3bWs4NyJ9.DMsirETTR4JOlIdu8dmizw&limit=1'
+//     console.log(url);
+//     request({url: url, json: true}, (error, response) => {
+//         if(error) {
+//             callback('Unable to connect location services!')
+//         }
+//         else if(response.body.message)
+//             callback('Please pass a valid location')
+//         else if(response.body.features.length === 0) {
+//             callback('Unable to find location. try another search.')
+//         }
+//         else{
+//             callback(undefined, {
+//                 latitude: response.body.features[0].center[1],
+//                 longitude: response.body.features[0].center[0],
+//                 location: response.body.features[0].place_name
+//             })
+//         }
+//     })
+// }
+
+
+
+// geoCode('Dehradun',(error, geoCodeData) => {
+//     if(error)
+//         return console.log(error)
+//     console.log(geoCodeData)
+//     forcast(geoCodeData.latitude, geoCodeData.longitude, (error, forecasrData) => {
+//         if(error)
+//             return console.log(error)
+//         console.log(geoCodeData.location)
+//         console.log(forecasrData)
+//     })
+// })
+
+// const forcast = (lat, long, callback) => {
+//     const url = 'http://api.weatherstack.com/current?access_key=3b01490a702d8d41d46cba938459868c&query=' + lat + ',' + long
+
+//     request({url: url, json: true}, (error, response) => {
+//         if(error)
+//         {
+//             callback('Unabke to connect to weather service')
+//         }
+//         else if(response.body.error)
+//         {
+//             callback('Unable to find weather location')
+//         }
+//         else
+//         {
+//             callback(undefined,response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature +' degrees out. It feels like ' + response.body.current.feelslike + ' degrees out.')
+//         }
+//     })
+// }
--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,22 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Delhi',(error, geoCodeData) => {
-    if(error) {
-        return console.log(error)
-    }
-    
-    forecast(geoCodeData.latitude, geoCodeData.longitude, (error, forecasrData) => {
+const address = process.argv[2]
+if(!address)
+    console.log('Please provide an address to get weather data');
+else{
+    geocode(address,(error, geoCodeData) => {
         if(error) {
             return console.log(error)
         }
-        console.log(geoCodeData.location)
-        console.log(forecasrData)
-      })
-})
+        
+        forecast(geoCodeData.latitude, geoCodeData.longitude, (error, forecasrData) => {
+            if(error) {
+                return console.log(error)
+            }
+            console.log(geoCodeData.location)
+            console.log(forecasrData)
+          })
+    })
+}
 
