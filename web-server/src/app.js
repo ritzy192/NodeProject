d054ed3a2a5aca10537c788881e1865cce552dbf
Rythem Datta <ritzy192@gmail.com>
2021-07-16 17:53:49 +0530

    installed hbs(handlebar plugin for express) and created dynamic templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,20 +1,40 @@
-
-const path = require('path')
-const express = require('express')
+const path = require('path');
+const express = require('express');
 const app = express();
 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public/about.html'))
+console.log(__dirname);
+console.log(path.join(__dirname, '../public/about.html'));
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'hbs title new title new new',
+    name: 'rythem',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'about title new',
+    name: 'rythem',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    msg: 'Help msg',
+  });
+});
 
 app.get('/weather', (req, res) => {
-    res.send({
-        location: 'Delhi',
-        forcast: 50
-    })
-})
+  res.send({
+    location: 'Delhi',
+    forcast: 50,
+  });
+});
 
-app.listen(3000, ()=>{
-    console.log('Server started at port no 3000');
+app.listen(3000, () => {
+  console.log('Server started at port no 3000');
 });
