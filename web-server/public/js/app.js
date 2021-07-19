e13a036e30273a808e525bc4d4517c22cf2a35ad
Rythem Datta <ritzy192@gmail.com>
2021-07-19 13:49:09 +0530

    used fetch() to access our weather end point

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,9 @@
-console.log("js file")
+console.log('js file new');
+fetch('http://localhost:3000/weather?address=jammu').then((response) => {
+  response.json().then((data) => {
+    if (data.error) return console.log(data.error);
+
+    console.log(data.forcast);
+    console.log(data.location);
+  });
+});
