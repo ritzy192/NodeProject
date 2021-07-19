afcec4bd2f3847bcd62ae2102dfcf41ef7f2d962
Rythem Datta <ritzy192@gmail.com>
2021-07-19 14:30:10 +0530

    created form to take dynamic location and logging weather in browser console

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,9 +1,18 @@
 console.log('js file new');
-fetch('http://localhost:3000/weather?address=jammu').then((response) => {
-  response.json().then((data) => {
-    if (data.error) return console.log(data.error);
 
-    console.log(data.forcast);
-    console.log(data.location);
-  });
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = search.value;
+  fetch('http://localhost:3000/weather?address=' + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) return console.log(data.error);
+
+        console.log(data.forcast);
+        console.log(data.location);
+      });
+    }
+  );
 });
